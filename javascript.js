a2ed1b20c032c6d91b4488a1d42a5117317b6049
Kevin Ayala <kevin@Kevins-MacBook-Air.local>
2021-06-21 14:12:07 -0700

    Changed multiple Div elements to a large table element and created constant variables

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/*----- constants -----*/
+// variables that are not going to be re-assigned
+
+let gridRow = document.getElementsByTagName('tr');
+let gridCell = document.getElementsByTagName('td');
+let circle = document.getElementsByClassName('grid-item');
+const player = document.getElementsByClassName('current-player');
+const result = document.getElementById('result')
+
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
